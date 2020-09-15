a582c6a88881b257d97e4b118be45a684787c2fc
gabrielandre-andre <silvagabrielandre7@gmail.com>
2020-09-15 16:00:22 +0000

    Update

--- Plataforma/js.js
+++ Plataforma/js.js
@@ -1,8 +1,70 @@
 
-//document.cookie = "nome=John Doe";
-//document.cookie = "s=1";
+const content = {}
+
+
+content.buttonback = document.getElementById('backvideo')
+content.buttonlater = document.getElementById('latervideo')
+content.checkVideo = document.getElementById('checkVideoWatch')
+
+content.videoContent = document.getElementById('video')
+
+
+var url   = window.location.search.replace("?", "");
+var items = url.split("&");
+
+var array = {
+  'video-id' : items[0],
+  'name' : items[1]
+}
+
+content.videoId1 = array["video-id"].split('=');
+content.videoId = parseInt(content.videoId1[1])
+
+if(content.videoId <= 0){
+    content.buttonback.style.backgroundColor = 'grey'
+    window.location = '?video-id=1'
+}else if(content.videoId == 1){
+    content.buttonback.style.backgroundColor = 'grey'
+}
+
+
+function latervideo(){
+    var calcVideo = content.videoId+1
+	window.location = '?video-id='+calcVideo;
+
+}
+function backvideo(){
+
+    var calcVideo = content.videoId-1
+	window.location = '?video-id='+calcVideo;
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 var x = document.cookie
+
+console.log(x)
 
 function valor_cookie(nome_cookie) {
     // Adiciona o sinal de = na frente do nome do cookie
@@ -31,6 +93,86 @@ function valor_cookie(nome_cookie) {
     return decodeURI(cookies);
 }
 
-if(valor_cookie('video-watch') == 1){
-    
+setInterval(function(){
+
+    if(content.checkVideo.checked == true){
+        document.cookie = content.videoId+'=1'
+    }if(content.checkVideo.checked != true){
+        document.cookie = content.videoId+'=2'
+    }
+
+
+},100)
+
+
+for(var i = 0; i <= 10; i++){
+    if(valor_cookie(i) == 1){
+
+        
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+videoSelect(content.videoId,3)
+
+
+
+function videoSelect(videoId,maxId){
+
+    if(videoId == 1){
+
+        content.videoContent.innerHTML = "<iframe src='https://www.youtube.com/embed/CrSC1ZA9j0M' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>"
+
+
+
+    }else if(videoId == 2){
+
+
+        content.videoContent.innerHTML = "<iframe src='https://www.youtube.com/embed/sVnfv3-SeuU' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>"
+
+
+    }
+
+
+
+
+
+
+
+    if(videoId >= maxId){
+        var VideoID = maxId - 1
+        window.location = '?video-id='+ VideoID;
+    }
+
+    if(videoId == maxId-1){
+        content.buttonlater.style.backgroundColor = 'grey'
+    }
+
 }
